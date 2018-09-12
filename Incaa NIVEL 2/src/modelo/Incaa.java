7ff084baef875dbc6e7f6105222e6428107be515
alan-PC\alan <alan.perez536@outlook.es>
2018-09-12 16:07:18 -0300

    Incaa nivel 2

---
+++ Incaa NIVEL 2/src/modelo/Incaa.java
@@ -0,0 +1,100 @@
+package modelo;
+import java.util.*;
+public class Incaa {
+	private List <Pelicula> catalogo;
+
+	public Incaa() throws Exception{
+		super();
+		this.catalogo = new ArrayList<Pelicula>();
+	}
+
+	public List<Pelicula> getCatalogo() {
+		return catalogo;
+	}
+
+	
+	
+	
+	public Pelicula traerPelicula(int idPelicula) {
+		Pelicula p = null;
+		int i = 0;
+		while(p== null && i < catalogo.size()) {
+			if(catalogo.get(i).getIdPelicula() == idPelicula)
+				p = catalogo.get(i);
+			i++;
+		}
+		return p;
+	}
+	
+	public List<Pelicula> traerpelicula(String partePelicula){
+		List<Pelicula> nuevaLista = new ArrayList<Pelicula>();
+		int i = 0;
+		while(i < catalogo.size()) {
+			if(catalogo.get(i).getPelicula().contains(partePelicula))
+				nuevaLista.add(catalogo.get(i));
+			i++;
+		}
+		return nuevaLista;
+		
+	}
+	public List<Pelicula> traerpelicula(Genero genero){
+		List<Pelicula> nuevaLista = new ArrayList<Pelicula>();
+		int i = 0;
+		while(i < catalogo.size()) {
+			if(catalogo.get(i).getGenero().equals(genero))
+				nuevaLista.add(catalogo.get(i));
+			i++;
+		}
+		return nuevaLista;
+		
+	}
+	
+	public Pelicula traerPeli(String pelicula) {
+		Pelicula p = null;
+		int i = 0;
+		while(p == null && i < catalogo.size() ) {
+			if(catalogo.get(i).getPelicula().equalsIgnoreCase(pelicula))
+				p = catalogo.get(i);
+			i++;
+		}
+		return p;
+	}
+	
+	public boolean agregarPelicula(String pelicula,Genero genero) throws Exception{
+		int proxId = 0;
+		if(traerPeli(pelicula) != null) throw new Exception ("ya esta agregado en la lista");
+		else {
+			if(catalogo.size() == 0)
+				proxId = 1;
+			else {
+				proxId = catalogo.get(catalogo.size()-1).getIdPelicula() + 1;
+			}
+		}
+		return catalogo.add(new Pelicula(proxId,pelicula,genero));
+	}
+	
+	public boolean eliminarPelicula(int idPelicula) throws Exception{;
+		Pelicula p = null;
+		if(traerPelicula(idPelicula) == null) throw new Exception("no existe la pelicula");
+		p = traerPelicula(idPelicula);
+		return catalogo.remove(p);
+	}
+	
+	public boolean modificarPelicula(int idPelicula,String pelicula,Genero genero) throws Exception {
+		boolean modificado = true;
+		int i =  0;
+		Pelicula p = null;
+		if(traerPelicula(idPelicula) == null) throw new Exception("no existe la pelicula a modificar");
+		p = traerPelicula(idPelicula);
+		p.setPelicula(pelicula);
+		p.setGenero(genero);
+		catalogo.set(idPelicula-1, p);
+		return modificado;
+	}
+
+	@Override
+	public String toString() {
+		return "Incaa" + catalogo + "";
+	}
+	
+}
---
+++ Incaa NIVEL 2/src/test/Test.java
@@ -0,0 +1,105 @@
+package test;
+import java.util.*;
+import modelo.*;
+public class Test {
+
+	public static void main(String[] args) {
+		Genero g1 = new Genero(1,"vampirico");
+		Genero g2 = new Genero(2,"comedia");
+		Genero g3 = new Genero(3,"accion");
+		// TODO Auto-generated method stub
+		try {
+			Incaa i = new Incaa();
+			
+
+			System.out.println(i.agregarPelicula("Harry Potter",g1));
+			System.out.println(i.agregarPelicula("El señor de los anillos",g2));
+			System.out.println(i.agregarPelicula("La monja",g3));
+			System.out.println(i.toString());
+			System.out.println();
+			
+			}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		
+		}
+		try {
+			Incaa i = new Incaa();
+			System.out.println("EXCEPTION 1");
+			System.out.println(i.agregarPelicula("Harry Potter",g1));
+			System.out.println(i.agregarPelicula("El señor de los anillos",g2));
+			System.out.println(i.agregarPelicula("La monja",g3));
+			System.out.println(i.agregarPelicula("Harry Potter",g3));
+			
+			
+			}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		
+			}
+		
+		try {
+			Incaa i = new Incaa();
+			System.out.println();
+			System.out.println(i.agregarPelicula("Harry Potter",g1));
+			System.out.println(i.agregarPelicula("El señor de los anillos",g2));
+			System.out.println(i.agregarPelicula("La monja",g3));
+			System.out.println(i.traerPeli("nose") );
+			System.out.println();
+			
+		}
+		
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		
+		}
+		try {
+			Incaa i = new Incaa();
+			System.out.println("EXCEPTION 3: ELIMINAR PELICULA QUE NO ESTE EN LA LISTA");
+			System.out.println(i.agregarPelicula("Harry Potter",g1));
+			System.out.println(i.eliminarPelicula(2));
+	
+			}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		
+		}
+		try {
+			Incaa i = new Incaa();
+			System.out.println(" ");
+			System.out.println(i.agregarPelicula("Harry Potter",g1));
+			System.out.println(i.agregarPelicula("Harry y el dragon",g2));
+			System.out.println(i.agregarPelicula("nose",g2));
+			System.out.println(i.traerpelicula("Harry"));
+			System.out.println(i.eliminarPelicula(2));
+			System.out.println(i.toString());
+			}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		
+		}
+		try {
+			Incaa i = new Incaa();
+			System.out.println();
+			System.out.println("agregar/eliminar/modificar pelicula");
+			System.out.println(i.agregarPelicula("Harry Potter",g1));
+			System.out.println(i.agregarPelicula("El señor de los anillos",g1));
+			System.out.println(i.agregarPelicula("La monja",g2));
+			System.out.println(i.toString());
+			System.out.println(i.traerPelicula(2));
+			System.out.println(i.modificarPelicula(3, "probando",g1));
+			System.out.println(i.toString());
+			System.out.println(i.agregarPelicula("Hercules",g3));
+			System.out.println(i.eliminarPelicula(3));
+			System.out.println(i.toString());
+			System.out.println(i.traerpelicula(g1));
+
+			
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		
+	}
+	}
+
+}
